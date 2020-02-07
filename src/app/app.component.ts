83c4bb1fd67c821119721acffc780c28b96defd0
InnocentTIALO <tialoinnocent@gmail.com>
2020-02-07 08:44:45 +0100

    mastering how to create custom validators for form controls of reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,12 @@ import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
 export class AppComponent implements OnInit{
   genders = ['male', 'female'];
   signupForm: FormGroup;
+  forbiddenUserNames = ['Anna', 'Sali', 'Bob', 'Joe'];
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenName.bind(this)]),
         'email': new FormControl(null, [Validators.required, Validators.email])
       }),
       'gender': new FormControl('male'),
@@ -32,5 +33,16 @@ export class AppComponent implements OnInit{
 
   get hobbies() {
     return this.signupForm.get('hobbies') as FormArray;
+  }
+
+  getFormControlWithKey(key: string) {
+    return this.signupForm.get(key);
+  }
+
+  forbiddenName(control: FormControl): {[key: string]: boolean} | null {
+    if (this.forbiddenUserNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbidden': true};
+    }
+    return null;
   }
 }
